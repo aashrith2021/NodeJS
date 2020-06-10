e26699454968122f3ef07cf91262ed4c70b344b7
aashrith2021 <aashrithkan2@gmail.com>
2020-06-10 20:15:13 +0530

    callback example

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,19 +1,17 @@
-var rect = {
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};
+var rect =require('./rectangle');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
+    rect(l,b, (err,rectangle)=>{
+        if (err) {
+            console.log("error",err.msg)
+        }
+        else {
+            console.log("the are of rect"+rectangle.area()+"  "+rectangle.perimeter)
+        }
+    });
+    console.log("this statement")
 }
 
 solveRect(2,4);
